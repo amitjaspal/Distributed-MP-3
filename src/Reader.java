4f30da187586376669b80c5ea748dc5dadbd3a9c
bharat1910 <bharat@Bharats-MacBook-Pro.local>
2014-05-06 12:07:03 -0500

    Correcting the format of search message

--- src/Reader.java
+++ src/Reader.java
@@ -181,13 +181,12 @@ public class Reader extends Thread {
                     Long unixTime = System.currentTimeMillis() / 1000L;
                     messageBuilder.append(" " + unixTime.toString());
                     messageBuilder.append(" " + processId.toString());
-                    Integer level = Integer.parseInt(tokens[2]);
                     
                     for(int i = 0;i<replicas;i++){
                             int node = (key +i) % totalProcesses;
                             int delay = averageDelays[i];
                             int portNumber = processToPort.get(node);
-                            message = messageBuilder.toString() + " " + i + " " + level.toString();
+                            message = messageBuilder.toString() + " " + i;
                             Sender h  = new Sender(message, delay, portNumber);
                             h.start();
                     }
